a4b910688fb75837954c294610bcda166614e1c2
gravityfargo <gravityfargo@pm.me>
2023-02-06 22:56:25 -0500

    finished the printf and scanf lines

--- labproblem.c
+++ labproblem.c
@@ -16,12 +16,16 @@ void main() {
 	
 	/* Edit this printf statement for part A, B and C */
     printf("Enter a number between 0 and 65535: ");
-    // scanf();
-	printf("lsbNum = %d\n", num);
-    printf("lsbNum = %d\n", num);
-    printf("Number = %d\n", num);
-    printf("Number in Hex is \n");
-    printf("The LSB is = %d\n", num);
+	// I mean the lab specified an unsigned int...so %u I guess lol
+    scanf("%u", &num);
+	printf("lsbNum = %u\n", num);
+    printf("lsbNum = %u\n", num);
+    printf("Number = %u\n", num);
+	// %04X = a hex number with four digits padded by 0s, X capitalizes the chars
+	// 32 == 0032 so add '0x' to the string before it.
+    printf("Number in Hex is 0x%04X\n", num);
+    printf("The LSB is = %u\n", num);
+	printf("The MSB is = %u\n", num);
 
 
 }
